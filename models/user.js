9ce02ad8195d7f339332c514f869e6db56e16666
fquijada <fquijada@celcom.cl>
2020-09-08 22:35:07 -0300

    Creado modelo de usuario

---
+++ models/user.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const uuidv1 = require("uuid/v1");
+const crypto = require("crypto");
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 50
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true
+        },
+        hashed_password: {
+            type: String,
+            required: true
+        },
+        salt: String,
+        role: {
+            type: Number,
+            default: 0
+        }
+    },
+    {timestamps: true}
+);
+
+userSchema.virtual("password")
+.set(function(password){
+    this._password = password;
+    this.salt = uuidv1();
+    this.hashed_password = this.encryptPassword(password);
+})
+.get(function() {
+    return this._password;
+});
+
+userSchema.methods = {
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+
+    encryptPassword: function(password) {
+        if (!password)
+            return "";
+        try {
+            return crypto
+                .createHmac("sha1", this.salt)
+                .update(password)
+                .digest("hex");
+        } catch (err) {
+            return "";
+        }
+    }
+};
+
+module.exports = mongoose.model("User", userSchema);
